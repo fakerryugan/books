89d7a9003353e205ce1e7ec714b41cf75ddff212
fakerryugan <fatkurirham55@gmail.com>
2024-11-25 14:10:47 +0700

    W11: Soal 15

--- lib/navigation_first.dart
+++ lib/navigation_first.dart
@@ -1,13 +1,55 @@
 import 'package:flutter/material.dart';
 
+class NavigationSecond extends StatefulWidget {
+  const NavigationSecond({super.key});
+  @override
+  State<NavigationSecond> createState() => _NavigationSecondState();
+}
+
+class _NavigationSecondState extends State<NavigationSecond> {
+  @override
+  Widget build(BuildContext context) {
+    Color color;
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Navigation Second Screen'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                  child: const Text('Red'),
+                  onPressed: () {
+                    color = Colors.red.shade700;
+                    Navigator.pop(context, color);
+                  }),
+              ElevatedButton(
+                  child: const Text('Green'),
+                  onPressed: () {
+                    color = Colors.green.shade700;
+                    Navigator.pop(context, color);
+                  }),
+              ElevatedButton(
+                  child: const Text('Blue'),
+                  onPressed: () {
+                    color = Colors.blue.shade700;
+                    Navigator.pop(context, color);
+                  }),
+            ],
+          ),
+        ));
+  }
+}
+
 class NavigationFirst extends StatefulWidget {
   const NavigationFirst({super.key});
 
   @override
-  NavigationFirstState createState() => NavigationFirstState();
+  State<NavigationFirst> createState() => _NavigationFirstState();
 }
 
-class NavigationFirstState extends State<NavigationFirst> {
+class _NavigationFirstState extends State<NavigationFirst> {
   Color color = Colors.blue.shade700;
 
   @override
@@ -28,5 +70,12 @@ class NavigationFirstState extends State<NavigationFirst> {
     );
   }
 
-  void _navigateAndGetColor(BuildContext context) {}
+  Future _navigateAndGetColor(BuildContext context) async {
+    color = await Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const NavigationSecond()),
+        ) ??
+        Colors.blue;
+    setState(() {});
+  }
 }
